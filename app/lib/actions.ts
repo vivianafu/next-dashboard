c2cf15b1854a87befe28bdb8b1bf4f7d02761b8c
Viviana Fu <viviana_fu@wistron.com>
2023-12-15 16:37:56 +0800

    feat(login): add login, sign-out

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
   errors?: {
@@ -102,7 +104,6 @@ export async function updateInvoice(id:string, prevState: State,formData: FormDa
 }
 
 export async function deleteInvoice(id: string) {
-
   // throw new Error('Failed to Delete Invoice');
 
   try {
@@ -114,5 +115,24 @@ export async function deleteInvoice(id: string) {
       message: 'Database Error: Failed to Delete Invoice.',
     };
   }
+}
 
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
