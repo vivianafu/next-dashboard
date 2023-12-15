9d692e5d946c78e74b341c6023a1db883e863545
Viviana Fu <viviana_fu@wistron.com>
2023-12-15 15:14:45 +0800

    feat(form-validation): add edit-form validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -32,7 +32,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
-               aria-describedby="customer-error"
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -69,6 +69,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
+                aria-describedby="amount-error"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -132,7 +133,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
           </div>
         </fieldset>
-        <div id="status-error" aria-live="polite" aria-atomic="true">
+        <div id="error" aria-live="polite" aria-atomic="true">
             {state?.message &&
                 <p className="mt-2 text-sm text-red-500" >
                   {state?.message}
